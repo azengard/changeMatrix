9c08df20f7b48eba9da0f6b7dcd9eb00424f4b9d
azengard <azengard.gmail.com>
2016-08-20 23:03:10 -0300

    Add error treatment at menu. Add 'S' command for save file.

--- matriz.py
+++ matriz.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+
 def read_sequence():  # Read and validate a sequence of commands.
     charValid = ("ICLVHKFSX")
     sqc = input("Digite um comando: ").upper()
@@ -9,11 +10,12 @@ def read_sequence():  # Read and validate a sequence of commands.
         if char == sqc[0] and not "":
             return sqc
     else:
-        print("Comando Inválido!\n")
+        print("\nComando Inválido!\n")
         return sqc
 
 
 def print_board(board):  # Print the Board.
+    print("\n")
     for row in board:
         print("".join(row))
     print("\n")
@@ -21,49 +23,49 @@ def print_board(board):  # Print the Board.
 
 def create_array(cmd):  # Create a array - 'I' Command.
     board = []
-    _, M, N = cmd
+    col, line = cmd
 
-    for x in range(int(N)):
-        board.append(["O"] * int(M))
+    for x in range(int(line)):
+        board.append(["O"] * int(col))
     return board
 
 
 def clean_array(board):  # Clean a array - 'C' Command.
-    for x in range(0, len(board) - 1):
-        for y in range(0, len(board[x]) - 1):
-            board[x][y] = "O"
+    for line in range(0, len(board)):
+        for col in range(0, len(board[line])):
+            board[line][col] = "O"
     return board
 
 
 def color_pixel(cmd, board):  # Change the color of one pixel - 'L' Command.
-    _, X, Y, C = cmd
+    col, line, color = cmd
 
-    board[int(Y) - 1][int(X) - 1] = C
+    board[int(line) - 1][int(col) - 1] = color
     return board
 
 
 def ver_pixel(cmd, board):  # Change the color of a column - 'V' Command.
-    _, X, Y1, Y2, C = cmd
+    col, lineIni, lineEnd, C = cmd
 
-    for ver in range(int(Y1) - 1, int(Y2)):
-        board[int(ver)][int(X) - 1] = C
+    for ver in range(int(lineIni) - 1, int(lineEnd)):
+        board[int(ver)][int(col) - 1] = C
     return board
 
 
 def hor_pixel(cmd, board):  # Change the color of a line - 'H' Command.
-    _, X1, X2, Y, C = cmd
+    colIni, colEnd, line, color = cmd
 
-    for hor in range(int(X1) - 1, int(X2)):
-        board[int(Y) - 1][int(hor)] = C
+    for hor in range(int(colIni) - 1, int(colEnd)):
+        board[int(line) - 1][int(hor)] = color
     return board
 
 
 def block_pixel(cmd, board):  # Change color of an entire block - 'K' Command.
-    _, X1, Y1, X2, Y2, C = cmd
+    colIni, lineIni, colEnd, lineEnd, color = cmd
 
-    for hor in range(int(X1) - 1, int(X2)):
-        for ver in range(int(Y1) - 1, int(Y2)):
-            board[int(ver)][int(hor)] = C
+    for hor in range(int(colIni) - 1, int(colEnd)):
+        for ver in range(int(lineIni) - 1, int(lineEnd)):
+            board[int(ver)][int(hor)] = color
     return board
 
 
@@ -78,72 +80,79 @@ def out_range(board, Y, X):  # Check if a cmd is out of list range.
 
 
 def fill_pixel(cmd, board):  # Fill a continuous region 'F' command.
-    F, X, Y, chgColor = cmd
+    col, line, chgColor = cmd
 
-    color = board[Y][X]
+    color = board[line][col]
 
-    if out_range(board, Y, X):
-        board[Y][X] = chgColor
-        if out_range(board, Y, X - 1):
-            if board[Y][X - 1] == color:
-                print(X - 1, Y)
-                fill_pixel([F, X - 1, Y, chgColor], board)
+    if out_range(board, line, col):
+        board[line][col] = chgColor
 
-        if out_range(board, Y, X + 1):
-            if board[Y][X + 1] == color:
-                print(X + 1, Y)
-                fill_pixel([F, X + 1, Y, chgColor], board)
+        if out_range(board, line, col - 1):
+            if board[line][col - 1] == color:
+                fill_pixel([col - 1, line, chgColor], board)
 
-        if out_range(board, Y - 1, X):
-            if board[Y - 1][X] == color:
-                print(X, Y - 1)
-                fill_pixel([F, X, Y - 1, chgColor], board)
+        if out_range(board, line, col + 1):
+            if board[line][col + 1] == color:
+                fill_pixel([col + 1, line, chgColor], board)
 
-        if out_range(board, Y + 1, X):
-            if board[Y + 1][X] == color:
-                print(X, Y + 1)
-                fill_pixel([F, X, Y + 1, chgColor], board)
+        if out_range(board, line - 1, col):
+            if board[line - 1][col] == color:
+                fill_pixel([col, line - 1, chgColor], board)
+
+        if out_range(board, line + 1, col):
+            if board[line + 1][col] == color:
+                fill_pixel([col, line + 1, chgColor], board)
 
     return board
 
 
+def save_array(name, board):  # Save the array with the 'S' command.
+    with open(name.lower(), "w") as my_file:
+        for item in board:
+            my_file.write("".join(item) + "\n")
+
+
 def main():
     while True:
-        cmd = read_sequence()
+        try:
+            cmd = read_sequence()
 
-        if cmd[0] == "X" and len(cmd) == 1:
-            break
+            if cmd[0] == "X":
+                break
 
-        elif cmd[0] == "I" and len(cmd) == 3:
-            board = create_array(cmd)
+            elif cmd[0] == "I":
+                board = create_array(cmd[1:3])
 
-        elif cmd[0] == "L" and len(cmd) == 4:
-            board = color_pixel(cmd, board)
+            elif cmd[0] == "L":
+                board = color_pixel(cmd[1:4], board)
 
-        elif cmd[0] == "V" and len(cmd) == 5:
-            board = ver_pixel(cmd, board)
+            elif cmd[0] == "V":
+                board = ver_pixel(cmd[1:5], board)
 
-        elif cmd[0] == "H" and len(cmd) == 5:
-            board = hor_pixel(cmd, board)
+            elif cmd[0] == "H":
+                board = hor_pixel(cmd[1:5], board)
 
-        elif cmd[0] == "K" and len(cmd) == 6:
-            board = block_pixel(cmd, board)
+            elif cmd[0] == "K":
+                board = block_pixel(cmd[1:6], board)
 
-        elif cmd[0] == "F" and len(cmd) == 4:
-            cmd[1] = int(cmd[1]) - 1
-            cmd[2] = int(cmd[2]) - 1
-            board = fill_pixel(cmd, board)
+            elif cmd[0] == "F":
+                cmd[1] = int(cmd[1]) - 1
+                cmd[2] = int(cmd[2]) - 1
+                board = fill_pixel(cmd[1:4], board)
 
-        elif cmd[0] == "S" and len(cmd) == 1:
-            pass
+            elif cmd[0] == "S":
+                save_array(cmd[1], board)
 
-        elif cmd[0] == "C" and len(cmd) == 1:
-            board = clean_array(board)
+            elif cmd[0] == "C":
+                board = clean_array(board)
 
-        else:
-            continue
+            else:
+                continue
 
-        print_board(board)
+            print_board(board)
+
+        except:
+            print("\nComando inválido!\n")
 
 
 if __name__ == '__main__':
